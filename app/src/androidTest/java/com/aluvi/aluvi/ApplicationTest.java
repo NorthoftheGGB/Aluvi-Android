ad64a7cdf6307f95f1642dd52fd3e9504cae5555
Matthew Xi <matt@winterroot.net>
2015-07-13 17:08:00 -0700

    Added model for Profile

--- app/src/androidTest/java/com/aluvi/aluvi/ApplicationTest.java
+++ app/src/androidTest/java/com/aluvi/aluvi/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.aluvi.aluvi;
+package com.aluvi.android;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/java/com/aluvi/android/model/Profile.java
@@ -0,0 +1,28 @@
+package com.aluvi.android.model;
+
+/**
+ * Created by matthewxi on 7/13/15.
+ */
+public class Profile {
+
+    String firstName;
+    String lastName;
+    String phone;
+    String email;
+
+    int commuterRefillAmountCents;
+    int commuterBalanceCents;
+    int commuterRefillEnabled;
+
+    String defaultCardToken;
+    String cardLastFour;
+    String cardBrand;
+
+    String recipientCardBrand;
+    String recipientCardLastFour;
+    String bankAccountName;
+
+    String smallImageUrl;
+    String largeImageUrl;
+
+}
