88bb34533c2e8523f4226e8bc8ebf9bb3fcbfce1
Matthew Xi <matt@winterroot.net>
2015-07-18 00:46:22 -0400

    Added push configuration, boiler plate code, and PushManager class

--- app/src/main/java/com/aluvi/android/application/AluviApplication.java
+++ app/src/main/java/com/aluvi/android/application/AluviApplication.java
@@ -1,12 +1,16 @@
 package com.aluvi.android.application;
 
 import android.app.Application;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.aluvi.android.api.AluviApi;
+import com.aluvi.android.application.push.PushManager;
 import com.aluvi.android.managers.CommuteManager;
 import com.aluvi.android.managers.UserStateManager;
 import com.aluvi.android.model.local.TicketLocation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 /**
  * Created by matthewxi on 7/14/15.
@@ -17,6 +21,7 @@ public class AluviApplication extends Application {
         super.onCreate();
 
         AluviRealm.initialize(this);
+        PushManager.setup(this);
         GlobalIdentifiers.initialize(this);
         AluviApi.initialize(this);
         UserStateManager.initialize(this);
@@ -57,5 +62,26 @@ public class AluviApplication extends Application {
     public void onTerminate() {
         super.onTerminate();
         AluviRealm.closeDefaultRealm();
+    }
+
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/aluvi/android/application/push/PushManager.java
@@ -0,0 +1,43 @@
+package com.aluvi.android.application.push;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+/**
+ * Created by matthewxi on 7/18/15.
+ */
+public class PushManager {
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    public static void setup(Context context){
+        if (PushManager.checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(context, RegistrationIntentService.class);
+            context.startService(intent);
+        }
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private static boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i("PushManager", "This device is not supported.");
+            }
+            return false;
+        }
+        return true;
+    }
+}
