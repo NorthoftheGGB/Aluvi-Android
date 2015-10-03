04962dec09f1b944f3ea14931c475a7d6b893deb
u3amar@hotmail.com <u3amar@gmail.com>
2015-10-03 13:42:14 -0700

    Updated support libraries. Updated material dialogs library. Updated GMS services library

--- app/src/apiDebug/java/com/aluvi/android/api/AluviApiConstants.java
+++
@@ -1,9 +0,0 @@
-package com.aluvi.android.api;
-
-/**
- * Created by usama on 7/28/15.
- */
-public interface AluviApiConstants {
-    String API_BASE_URL = "http://52.24.74.155:3000/api/"; // Dev server
-
-}
