6aea2ff67aa44d73c74da94ff10fc3978279aa1d
u3amar@hotmail.com <u3amar@gmail.com>
2015-10-15 17:01:50 -0700

    Updated keys for alpha build

---
+++ app/src/alpha/java/com/aluvi/android/api/AluviApiConstants.java
@@ -0,0 +1,8 @@
+package com.aluvi.android.api;
+
+/**
+ * Created by usama on 7/28/15.
+ */
+public interface AluviApiConstants {
+    String API_BASE_URL = "http://52.27.239.209:3000/api/"; // Alpha server
+}
